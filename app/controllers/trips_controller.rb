0762febbb461e3a679cafce80b5dfcd77bda1b47
RobynSpaulding <robynm27@gmail.com>
2022-11-07 13:03:31 -0800

    sets user_id to current_user.id rather than params in trips controller create action, removes user_id params from update action

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,6 +1,6 @@
 class TripsController < ApplicationController
   before_action :authenticate_user
-  
+
   def index 
     trips = Trip.all 
     render json: trips.as_json
@@ -8,7 +8,7 @@ class TripsController < ApplicationController
 
   def create
     trip = Trip.new(
-      user_id: params[:user_id],
+      user_id: current_user.id,
       title: params[:title],
       image_url: params[:image_url],
       start_time: params[:start_time],
@@ -25,11 +25,12 @@ class TripsController < ApplicationController
 
   def update
     trip = Trip.find_by(id: params[:id])
-    trip.user_id = params[:user_id] || trip.user_id
+
     trip.title = params[:title] || trip.title
     trip.image_url = params[:image_url] || trip.image_url
     trip.start_time = params[:start_time] || trip.start_time
     trip.end_time = params[:end_time] || trip.end_time
+    
     trip.save
     render json: trip.as_json 
   end
