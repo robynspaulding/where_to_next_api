f94777dc92cf3cc223dd9d6bc2932990bd756658
RobynSpaulding <robynm27@gmail.com>
2022-11-07 12:27:17 -0800

    adds create action to trips controller, tests trips create action in request file

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -5,4 +5,16 @@ class TripsController < ApplicationController
     render json: trips.as_json
   end
 
+  def create
+    trip = Trip.new(
+      user_id: params[:user_id],
+      title: params[:title],
+      image_url: params[:image_url],
+      start_time: params[:start_time],
+      end_time: params[:end_time]
+    )
+    trip.save
+    render json: trip.as_json
+  end
+
 end
