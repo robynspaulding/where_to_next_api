11f071c52385086d7a6ec40d6816a0c72704e4f9
RobynSpaulding <robynm27@gmail.com>
2022-11-07 13:28:37 -0800

    adds restful API actions to places controller, adds places routes and tests each route and action in request file

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,2 +1,47 @@
 class PlacesController < ApplicationController
+
+  def index 
+    places = Place.all 
+    render json: places.as_json
+  end
+
+  def create
+    place = Place.new(
+      trip_id: params[:trip_id],
+      address: params[:address],
+      name: params[:name],
+      description: params[:description],
+      image_url: params[:image_url],
+      start_time: params[:start_time],
+      end_time: params[:end_time]
+    )
+    place.save
+    render json: place.as_json
+  end
+
+  def show
+    place = Place.find_by(id: params[:id])
+    render json: place.as_json
+  end
+
+  def update
+    place = Place.find_by(id: params[:id])
+
+    place.address = params[:address] || place.address
+    place.name = params[:name] || place.name
+    place.description = params[:description] || place.description
+    place.image_url = params[:image_url] || place.image_url
+    place.start_time = params[:start_time] || place.start_time
+    place.end_time = params[:end_time] || place.end_time
+    
+    place.save
+    render json: place.as_json 
+  end
+
+  def destroy
+    place = Place.find_by(id: params[:id])
+    place.destroy
+    render json: {message: "place successfully deleted"}
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,10 @@ Rails.application.routes.draw do
   get "/trips/:id" => "trips#show"
   patch "/trips/:id" => "trips#update"
   delete "/trips/:id" => "trips#destroy"
+  # Places Routes
+  get "/places" => "places#index"
+  post "/places" => "places#create"
+  get "/places/:id" => "places#show"
+  patch "/places/:id" => "places#update"
+  delete "/places/:id" => "places#destroy"
 end
