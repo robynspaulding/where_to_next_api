3714855eaffa3a45d9e903fef538de980373c865
RobynSpaulding <robynm27@gmail.com>
2022-11-09 10:03:16 -0800

    installs jb gem, trips index and show views, renders @trip templates in trips controller

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,12 +1,12 @@
 class PlacesController < ApplicationController
 
   def index 
-    places = Place.all 
-    render json: places.as_json
+    @places = Place.all 
+    render json: @places.as_json
   end
 
   def create
-    place = Place.new(
+    @place = Place.new(
       trip_id: params[:trip_id],
       address: params[:address],
       name: params[:name],
@@ -15,32 +15,32 @@ class PlacesController < ApplicationController
       start_time: params[:start_time],
       end_time: params[:end_time]
     )
-    place.save
-    render json: place.as_json
+    @place.save
+    render json: @place.as_json
   end
 
   def show
-    place = Place.find_by(id: params[:id])
-    render json: place.as_json
+    @place = Place.find_by(id: params[:id])
+    render json: @place.as_json
   end
 
   def update
-    place = Place.find_by(id: params[:id])
+    @place = Place.find_by(id: params[:id])
 
-    place.address = params[:address] || place.address
-    place.name = params[:name] || place.name
-    place.description = params[:description] || place.description
-    place.image_url = params[:image_url] || place.image_url
-    place.start_time = params[:start_time] || place.start_time
-    place.end_time = params[:end_time] || place.end_time
+    @place.address = params[:address] || @place.address
+    @place.name = params[:name] || @place.name
+    @place.description = params[:description] || @place.description
+    @place.image_url = params[:image_url] || @place.image_url
+    @place.start_time = params[:start_time] || @place.start_time
+    @place.end_time = params[:end_time] || @place.end_time
     
-    place.save
-    render json: place.as_json 
+    @place.save
+    render json: @place.as_json 
   end
 
   def destroy
-    place = Place.find_by(id: params[:id])
-    place.destroy
+    @place = Place.find_by(id: params[:id])
+    @place.destroy
     render json: {message: "Place successfully deleted"}
   end
 
--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -2,42 +2,42 @@ class TripsController < ApplicationController
   before_action :authenticate_user
 
   def index 
-    trips = Trip.all 
-    render json: trips.as_json
+    @trips = Trip.all 
+    render template: "trips/index"
   end
 
   def create
-    trip = Trip.new(
+    @trip = Trip.new(
       user_id: current_user.id,
       title: params[:title],
       image_url: params[:image_url],
       start_time: params[:start_time],
       end_time: params[:end_time]
     )
-    trip.save
-    render json: trip.as_json
+    @trip.save
+    render template: "trips/show"
   end
 
   def show
-    trip = Trip.find_by(id: params[:id])
-    render json: trip.as_json
+    @trip = Trip.find_by(id: params[:id])
+    render template: "trips/show" 
   end
 
   def update
-    trip = Trip.find_by(id: params[:id])
+    @trip = Trip.find_by(id: params[:id])
 
-    trip.title = params[:title] || trip.title
-    trip.image_url = params[:image_url] || trip.image_url
-    trip.start_time = params[:start_time] || trip.start_time
-    trip.end_time = params[:end_time] || trip.end_time
+    @trip.title = params[:title] || @trip.title
+    @trip.image_url = params[:image_url] || @trip.image_url
+    @trip.start_time = params[:start_time] || @trip.start_time
+    @trip.end_time = params[:end_time] || @trip.end_time
     
-    trip.save
-    render json: trip.as_json 
+    @trip.save
+    render template: "trips/show" 
   end
 
   def destroy
-    trip = Trip.find_by(id: params[:id])
-    trip.destroy
+    @trip = Trip.find_by(id: params[:id])
+    @trip.destroy
     render json: {message: "Trip successfully deleted"}
   end
 
